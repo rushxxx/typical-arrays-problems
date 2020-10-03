c55267a79b3463a9af39dc62b29119d42a68afae
Ruslan <rush.yu@gmail.com>
2020-10-03 08:12:34 +0500

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min
+    if (Array.isArray(array) && array.length) {
+        min = array[0];
+        for (elem of array) {
+            if (elem < min) {
+                min = elem
+            }
+        }
+    }else {
+        return 0
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return 0
 }
